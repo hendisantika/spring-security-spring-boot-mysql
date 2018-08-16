06f193b2904177e9501b6dee7d4b376a2c41dae7
Hendi Santika <hendisantika@gmail.com>
2018-08-17 06:24:00 +0700

    #15 - Modify Main App

--- src/main/java/com/hendisantika/springsecurityspringbootmysql/SpringSecuritySpringBootMysqlApplication.java
+++ src/main/java/com/hendisantika/springsecurityspringbootmysql/SpringSecuritySpringBootMysqlApplication.java
@@ -1,12 +1,48 @@
 package com.hendisantika.springsecurityspringbootmysql;
 
+import com.hendisantika.springsecurityspringbootmysql.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringSecuritySpringBootMysqlApplication {
+public class SpringSecuritySpringBootMysqlApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecuritySpringBootMysqlApplication.class, args);
+    }
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Autowired
+    private UserService userService;
+
+    /*
+     * This method will run during application startup and execute all the codes inside this method
+     *
+     */
+    @Override
+    public void run(String... arg0) {
+        //Remove or comment this part after first execution of application,
+        //or else duplicate data will be inserted in the database
+		/*
+		AppUser admin = new AppUser();
+		admin.setActive(true);
+		admin.setPassword(encoder.encode("password"));
+		admin.setUsername("admin");
+		admin.setRoles(Arrays.asList(new Role("ADMIN")));
+		userService.saveUser(admin);
+
+		AppUser user = new AppUser();
+		user.setActive(true);
+		user.setPassword(encoder.encode("password"));
+		user.setUsername("user");
+		user.setRoles(Arrays.asList(new Role("USER")));
+		userService.saveUser(user);
+		*/
+
     }
 }

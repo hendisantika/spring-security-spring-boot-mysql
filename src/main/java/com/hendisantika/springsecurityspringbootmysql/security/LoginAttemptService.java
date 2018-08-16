3deb9c90d48fc20fff7d916f36e220c6c687a267
Hendi Santika <hendisantika@gmail.com>
2018-08-17 06:20:41 +0700

    #11 - Add Login Attempt Service

---
+++ src/main/java/com/hendisantika/springsecurityspringbootmysql/security/LoginAttemptService.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springsecurityspringbootmysql.security;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-spring-boot-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/08/18
+ * Time: 06.20
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginAttemptService {
+}
